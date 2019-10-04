98edd72ad6b211e5bc1293748dd6999cf6848bdb
Iamverygood88 <e_sindiukova@fanshaweonline.ca>
2019-10-04 11:26:25 -0400

    another try

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     uname: 'root',
     upass: 'root',
-    port:  process.env.PORT || 8889,
+    port:  'process.env.PORT' || 8889,
     host: 'localhost',
     db: 'bizcard_sindiukova'
 }
