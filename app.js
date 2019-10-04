24232c4b8b8e87df8cdd5b3b5805f8e1ba0e60c5
Iamverygood88 <e_sindiukova@fanshaweonline.ca>
2019-10-03 21:12:57 -0400

    fixed app.js

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.static('public'));
+
+
 app.get('/', (req, res) => {
-    res.send('hello world!');
+    res.sendFile(path.join(__dirname + '/views/index.html'));
 })
 
 app.get('/contact', (req, res) => {
-    res.send('on the contact page!');
+    res.sendFile(path.join(__dirname + '/views/contact.html'));
 })
 
 app.get('/portfolio', (req, res) => {
